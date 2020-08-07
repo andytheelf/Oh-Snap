01e31f81d0b5b2f2fd7b2a2463edace04d546453
andytheelf <ballew22@gmail.com>
2020-08-06 20:04:59 -0500

    second tests

--- photo-port/src/components/Nav/index.js
+++ photo-port/src/components/Nav/index.js
@@ -1,13 +1,13 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import { capitalizeFirstLetter } from '../../utils/helpers';
+import React, { useEffect } from 'react';
+
 
 function Nav(props) {
   const {
     categories = [],
     setCurrentCategory,
     currentCategory,
-    contactSelected,
-    setContactSelected
   } = props;
 
   useEffect(() => {
@@ -24,23 +24,23 @@ function Nav(props) {
       <nav>
         <ul className="flex-row">
           <li className="mx-2">
-          <a data-testid="about" href="#about" onClick={() => setContactSelected(false)}>
+            <a data-testid="about" href="#about">
               About me
             </a>
           </li>
-          <li className={`mx-2 ${contactSelected && 'navActive'}`}>
-          <span onClick={() => setContactSelected(true)}>Contact</span>
+          <li className="mx-2">
+            <span>Contact</span>
           </li>
           {categories.map((category) => (
             <li
-            className={`mx-1 ${
-              currentCategory.name === category.name && !contactSelected && `navActive`
-              }`}
-            key={category.name}
-          >
-              <span onClick={() => {
-                setCurrentCategory(category);
-                setContactSelected(false);
+              className={`mx-1 ${
+                currentCategory.name === category.name && 'navActive'
+                }`}
+              key={category.name}
+            >
+              <span
+                onClick={() => {
+                  setCurrentCategory(category)
                 }}
               >
                 {capitalizeFirstLetter(category.name)}
